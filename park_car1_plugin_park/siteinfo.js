de99161a3546bea4bbd505d90cd1f3fdae925ca9
fagegee <5408040+fagegee@user.noreply.gitee.com>
2020-10-16 16:09:00 +0800

    微擎停车小程序插件

---
+++ park_car1_plugin_park/siteinfo.js
@@ -0,0 +1,4 @@
+var siteinfo = { 
+	"m": 'park_car1_plugin_park',
+	"uniacid": "4", "acid": "4", "multiid": "0", "version": "1.0", "siteroot": "https://weixin.www7.com/app/index.php", "design_method": "3" }
+module.exports = siteinfo
